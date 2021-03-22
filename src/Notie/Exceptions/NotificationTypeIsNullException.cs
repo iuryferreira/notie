f6500bc02f0748265c5e1bb5fdb5971140173749
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-03-22 14:35:13 -0300

    ✨ add main exceptions

---
+++ src/Notie/Exceptions/NotificationTypeIsNullException.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Notie.Exceptions
+{
+    /// <summary>
+    /// The exception is thrown when a null notification type is passed to the method that does not allow this operation.
+    /// </summary>
+    public class NotificationTypeIsNullException : ArgumentNullException
+    {
+        private new const string Message = "Notification type cannot be null.";
+        /// <summary>
+        /// Initialize a new instance of NotificationTypeIsNullException with default message.
+        /// </summary>
+        public NotificationTypeIsNullException () : base(Message) { }
+    }
+}
---
+++ src/Notie/Exceptions/ValidationResultIsNullException.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Notie.Exceptions
+{
+    /// <summary>
+    /// The exception is thrown when a null validation result is passed to the method that does not allow this operation.
+    /// </summary>
+    public class ValidationResultIsNullException : Exception
+    {
+        private new const string Message = "Notification cannot be null.";
+        /// <summary>
+        /// Initialize a new instance of ValidationResultIsNullException with default message.
+        /// </summary>
+        public ValidationResultIsNullException () : base(Message) { }
+    }
+}
