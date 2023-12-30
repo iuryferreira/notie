19209cd6cec9739891ae4b758d12d654b0f641f7
iuryferreira <iury.franklinferreira@gmail.com>
2023-12-30 14:08:37 -0300

    👷🏽 fix readme.md file

--- src/Notie/Models/Notification.cs
+++ src/Notie/Models/Notification.cs
@@ -2,8 +2,6 @@ namespace Notie.Models;
 
 /// <summary>
 ///     This class define Notification object.
-///     <param name="Key">The validation key or field.</param>
-///     <param name="Message">Validation message for entered key.</param>
 /// </summary>
 public class Notification
 {
